2d4fd0ef9b11f2b7f366571fa6101b1cbdf32062
ZIN MYO SWE <overmidnight12@gmail.com>
2026-01-07 14:10:19 +0700

    add landingpage style

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,23 @@ const page = () => {
   return (
     <div>
       <Header />
-      Home Page
+      <main className='flex-1 flex-col items-center px-4 py-16 sm:px-6 text-center gap-20'>
+        <div className='max-w-4xl space-y-8 relative'>
+
+          {/* Background Gradient */}
+          <div className='absolute inset-0 -z-10 bg-gradient-to-br from-blue-50 via-indigo-50 to-purple-50
+          dark:from-blue-20 dark:via-indigo-950/20 dark:to-purple-950/20 rounded-3xl scale-150 opacity-60'></div>
+
+
+        </div>
+        <h1 className='text-4xl font-bold'>Connect Instantly
+          <br />
+          <span>
+            Chat Smarter
+          </span>
+        </h1>
+    
+      </main>
     </div>
   )
 }
