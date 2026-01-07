c8ce56027590c2cb81b74368c36ec871a2bde654
ZIN MYO SWE <overmidnight12@gmail.com>
2026-01-07 08:53:43 +0700

    Add Shadcn and Clerk Auth

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,12 @@
 import type { Metadata } from "next";
+import {
+  ClerkProvider,
+  SignInButton,
+  SignUpButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from '@clerk/nextjs'
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
@@ -27,7 +35,13 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <ClerkProvider
+          signInFallbackRedirectUrl="/dashboard"
+          signUpFallbackRedirectUrl="/dashboard"
+        >
+           {children}
+        </ClerkProvider>
+       
       </body>
     </html>
   );
