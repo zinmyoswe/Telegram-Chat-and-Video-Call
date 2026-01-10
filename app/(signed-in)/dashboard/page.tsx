14d348890c7ee0dad228f0ce240e5e6069f63ab9
ZIN MYO SWE <overmidnight12@gmail.com>
2026-01-10 12:18:31 +0700

    Implement shadcn sidebar and dashboard page

--- app/(signed-in)/dashboard/page.tsx
+++ app/(signed-in)/dashboard/page.tsx
@@ -1,9 +1,16 @@
+import { useSidebar } from '@/components/ui/sidebar';
+import { useUser } from '@clerk/nextjs';
+import { useRouter } from 'next/router';
 import React from 'react'
+import { useChatContext } from 'stream-chat-react';
 
-const Dashboard = () => {
-  return (
-    <div>Dashboard</div>
-  )
+function Dashboard() {
+  const { user } = useUser();
+  const router = useRouter();
+  const { channel, setActiveChannel } = useChatContext();
+  const { setOpen } = useSidebar();
+
+  return <div>Dashboard</div>
 }
 
 export default Dashboard
--- app/(signed-in)/layout.tsx
+++ app/(signed-in)/layout.tsx
@@ -1,11 +1,50 @@
 'use client'
 import UserSyncWrapper from '@/components/UserSyncWrapper'
 import React from 'react'
+import { Chat } from 'stream-chat-react';
+import streamClient from '@/lib/stream';
+import { SidebarInset, SidebarProvider, SidebarTrigger } from '@/components/ui/sidebar';
+import { AppSidebar } from '@/components/app-sidebar';
+import { Separator } from '@radix-ui/react-separator';
+
+import Link from 'next/link';
+
 
 function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <UserSyncWrapper>{children}</UserSyncWrapper>
-    )   
+    <UserSyncWrapper>
+      <Chat client={streamClient}>
+            <SidebarProvider
+      style={
+        {
+          "--sidebar-width": "19rem",
+        } as React.CSSProperties
+      }
+    >
+      <AppSidebar />
+      <SidebarInset>
+        <header className="flex h-16 shrink-0 items-center gap-2 px-4">
+          <SidebarTrigger className="-ml-1" />
+          <Separator
+            orientation="vertical"
+            className="mr-2 data-[orientation=vertical]:h-4"
+          />
+          
+          <Link href="/dashboard" >
+            <h1 className='text-lg font-bold tracking-wider uppercase text-black'>Telegramzin</h1>
+            
+          </Link>
+          
+        </header>
+        <div className='flex flex-1 flex-col gap-4 p-4 pt-0'>
+          {children}
+        </div>
+      </SidebarInset>
+    </SidebarProvider>
+      </Chat>
+      
+    </UserSyncWrapper>
+  )
 }
 
 export default Layout
